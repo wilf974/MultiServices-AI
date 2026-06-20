08dd6e771ae7c5397658a8768f131db685a89504
wilf974 <jean.maillot14@gmail.com>
2026-06-20 15:45:58 +0200

    docs: videos de lancement Remotion (DunkBot + memoire/licence) + GIF hero README

--- video/src/Root.tsx
+++ video/src/Root.tsx
@@ -1,15 +1,26 @@
 import { Composition } from "remotion";
 import { Demo, FPS, DURATION } from "./Video";
+import { License, LIC_FPS, LIC_DURATION } from "./License";
 
 export const RemotionRoot: React.FC = () => {
   return (
-    <Composition
-      id="Demo"
-      component={Demo}
-      durationInFrames={DURATION}
-      fps={FPS}
-      width={1920}
-      height={1080}
-    />
+    <>
+      <Composition
+        id="Demo"
+        component={Demo}
+        durationInFrames={DURATION}
+        fps={FPS}
+        width={1920}
+        height={1080}
+      />
+      <Composition
+        id="License"
+        component={License}
+        durationInFrames={LIC_DURATION}
+        fps={LIC_FPS}
+        width={1920}
+        height={1080}
+      />
+    </>
   );
 };
